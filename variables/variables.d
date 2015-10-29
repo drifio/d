5adffa93da8cef6f6cb10e560636219ccfb52799
Drif <drif@drif.io>
2015-10-29 12:24:52 +0000

    Exercises from the variables chapter

---
+++ variables/variables.d
@@ -0,0 +1,10 @@
+import std.stdio;
+
+void main()
+{
+    int studentCount;
+    writeln("There are ", studentCount, " students.");
+
+    studentCount = 420;
+    writeln("There are now ", studentCount, " students.");
+}
---
+++ variables/variables2.d
@@ -0,0 +1,10 @@
+import std.stdio;
+
+void main()
+{
+    int numOfEuros = 20;
+    float exchangeRate = 2.11;
+
+    writeln("I have exchanged ", numOfEuros, " Euros at the rate of ", exchangeRate, ".");
+
+}
